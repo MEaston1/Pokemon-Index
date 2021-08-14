c26bd7caffcd39c3d7ed40947b61624d5d98376c
MEaston1 <mattheweaston21@gmail.com>
2021-08-14 15:44:32 +0100

    small databinding changes
    Addition of the databinding adapter and small changes to various files

--- app/src/main/java/com/view/pokemonindex/model/PokemonModel.kt
+++ app/src/main/java/com/view/pokemonindex/model/PokemonModel.kt
@@ -2,4 +2,4 @@ package com.view.pokemonindex.model
 
 import java.io.Serializable
 
-class PokemonModel(val imageResId: Int, val name: String, val abilities: String, val health: Int, val species: String) : Serializable
+data class PokemonModel(val imageResId: Int, val name: String, val abilities: String, val health: Int, val species: String) : Serializable
